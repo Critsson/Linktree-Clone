c8e4f18eec0260b21b2fb49ab04b30be95ad59a3
Jason Gao <jj689x@gmail.com>
2022-12-29 21:56:22 -0500

    another comment

--- server/database/db.sql
+++ server/database/db.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password TEXT NOT NULL,
+    -- this should likely be a separate table that has a relationship to users
     links jsonb,
     bgcolor CHAR(6) NOT NULL DEFAULT 'ffffff',
     fontcolor CHAR(6) NOT NULL DEFAULT 'ffffff',
